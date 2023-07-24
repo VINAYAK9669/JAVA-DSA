266d23ab1e9f911f84e1450e763df848f3e6bb68
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-07-24 10:40:06 +0530

    Update 1. Generic Method Intro.java

--- 1. Generic Methods/1. Generic Method Intro.java
+++ 1. Generic Methods/1. Generic Method Intro.java
@@ -1,29 +1,20 @@
+// Program to Introduce Genric Method in java
+
 public class Main
 {
-
-  static class DemoClass < T >
-  {				// Make DemoClass a static inner class and add generic type parameter
-    void genericPrint (T t)
+    // Define a generic method , this is the syntax for the generic method
+    public <T> void GenericMethod(T parameter)
     {
-
-      System.out.println (t);
-
+        System.out.println(parameter);
     }
-  }
-
-  public static void main (String[]args)
-  {
-
-    DemoClass < Integer > aObj = new DemoClass <> ();	// Create an instance of DemoClass with Integer type
-    aObj.genericPrint (101);
-
-
-    DemoClass < String > bObj = new DemoClass <> ();	// Create an instance of DemoClass with String type
-    bObj.genericPrint ("Hello");
-
-
-    DemoClass < Double > cObj = new DemoClass <> ();	// Create an instance of DemoClass with Double type
-    cObj.genericPrint (3.14);
-
-  }
+    public static void main(String args[])
+    {
+        // We will create an instnce of the class i,e Object
+        Main object=new Main();
+      
+        // Now we will call method of the class Main with passing arguments
+        object.GenericMethod(5);   // passed an integer value
+        object.GenericMethod("Vinayak"); // passsed String
+        object.GenericMethod(5.677); // passed double value
+    }
 }
