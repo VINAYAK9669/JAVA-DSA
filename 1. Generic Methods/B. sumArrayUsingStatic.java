877f57e54cef5cafd25a763501e456ac57a2cbb5
VINAYAK9669 <51508724+VINAYAK9669@users.noreply.github.com>
2023-08-06 09:53:53 +0530

    Update B. sumArrayUsingStatic.java

--- 1. Generic Methods/B. sumArrayUsingStatic.java
+++ 1. Generic Methods/B. sumArrayUsingStatic.java
@@ -9,13 +9,20 @@
 
 public class Main {
     // Generic method to calculate the sum of elements in an array of any numeric type
+    // <T extends Number> - implements only the Number class like Integer, Double, Float.
     public static <T extends Number> Integer sumArray(T[] array) {
         // this fucntion returns an Interger hence need to consider Integer varibale to get the summation of array elements
         Integer sum=0;
         for(T element:array)
         {
             sum += element.intValue();
-            // element.intValue()- is used to convert the current element into an integer value
+            // element.intValue()- is used to convert the current element into an Integer value
+                    /*
+                        Even though the sum variable is of type Integer, Java handles the conversion between Integer and int automatically through "autounboxing" and "reboxing", allowing you 
+                        to perform arithmetic operations on Integer objects as if they were primitive int values.
+                            
+                        This is done behind the scenes by the Java compiler. 
+                    */
         }
         return sum;
     }
